3642ac985244b541cd02119d738afae38556601a
binhaji10 <binhaji00@gmail.com>
2026-06-21 11:48:39 +0100

    first commit

---
+++ ch2-Variables_DataTypes/script.js
@@ -0,0 +1,17 @@
+var town = "london" //string
+console.log(town)
+
+var FirstName = "khalid" //string
+console.log (FirstName)
+
+var LastName = "Elmi" //string
+console.log (LastName)
+
+var Age =(29) // number
+console.log("Age")
+
+
+ console.log(Age + 5)
+
+var isStudent = "true" //boolean
+console.log(true)
