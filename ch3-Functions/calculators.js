1aa728b7402c8a64e0885c09d49609a5c237220b
binhaji10 <binhaji00@gmail.com>
2026-07-08 09:34:46 +0100

    operators

---
+++ ch3-Functions/calculators.js
@@ -0,0 +1,57 @@
+// // function multiply(num1 ,num2 ){
+    
+// //     return num1 * num2
+// // }
+
+
+// var multiply = function(num1,num2){ // this is a traditional  version of creating function
+//     return num1 * num2
+// }
+
+// var result1 = multiply(3,2)
+// console.log(result1)
+// var result2 = multiply(3,3)
+// console.log result2
+
+
+// function add(num1 ,num2){
+// return num1 + num2
+
+// }
+// var result1 = add(5 , 8)
+// console.log(result1)
+
+function subtraction(num1,num2){
+    return num1-num2
+}
+ var result3 = subtraction(9,7)
+
+console.log(result3)
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+   
+
+
+
+ 
+ 
+  
---
+++ ch3-Functions/script.js
@@ -0,0 +1,28 @@
+function days(){
+console.log("Mon")
+console.log("Tue")
+console.log("Wed")
+console.log("Thu")
+console.log("Fri")
+console.log("Sat")
+console.log("Sun")
+}
+
+function months(params){
+    
+
+console.log("Jan")
+console.log("Feb")
+console.log("Mar")
+console.log("Apr")
+console.log("May")
+console.log("jun")
+console.log("jul")
+console.log("Aug")
+console.log("Sep")
+console.log("Oct")
+console.log("Nov")
+console.log("Dec")
+}
+
+days()
