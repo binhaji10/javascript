c5397d5381c2185d6de4dd1a67a78f69a3ec04c5
binhaji10 <binhaji00@gmail.com>
2026-07-09 19:33:09 +0100

    i Added Assignment operator

---
+++ ch4-Operators/Assignment.js
@@ -0,0 +1,14 @@
+var age = 30;
+// age++
+age--
+// age = age + 4
+// age += 4
+// age = age - 4
+// age -= 4
+// age = age * 4
+// age *= 4
+// age = age / 4
+// age /=4
+// age = age % 4
+// age %= 4
+console.log(age);
